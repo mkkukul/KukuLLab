583529a45f8eb65d119bdbcf740ce3746f1664e8
mkkukul <mkkukul@gmail.com>
2024-09-08 21:45:01 +0300

    9

--- Python/2.bölüm/str2.py
+++ Python/2.bölüm/str2.py
@@ -1,3 +1,9 @@
-isim = "tuncay"
-isim1 = 45
-print(type(isim1))
+# isim = "tuncay"
+# isim1 = 45
+# print(type(isim1))
+isim = "ali"
+d_yili = 1991
+yil = 2021
+yasHesapla = yil-d_yili
+
+print(yasHesapla)
