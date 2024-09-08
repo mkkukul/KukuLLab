7997bd7f384660449b037d22780ce88ac6ef4a49
mkkukul <mkkukul@gmail.com>
2024-09-08 22:23:43 +0300

    18

--- Python/2.bölüm/str4.py
+++ Python/2.bölüm/str4.py
@@ -2,3 +2,6 @@ buyukHarfler  = "NE MUTLU TÜRKÜM DİYENE".lower()
 kucukHarfler = "ne mutlu turkum diyene".upper()
 sadeceBasHarfliBuyuk = "ne mutlu".capitalize()
 tamTersi = "kardesim".swapcase()
+tamTersi = " KARDESİM"
+tamTersi = tamTersi.swapcase()
+sil = "+++gulyabini+++".strip("+")
