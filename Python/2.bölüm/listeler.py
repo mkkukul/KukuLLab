12bcf6ee8214674565846847d0ddfe92c5e9961f
mkkukul <mkkukul@gmail.com>
2024-09-09 19:15:09 +0300

    18

--- Python/2.bölüm/listeler.py
+++ Python/2.bölüm/listeler.py
@@ -1,4 +1,5 @@
 listeler = ["kurt","yakup","trabzon",3500]
 # print(len(listeler))
 # print(listeler[0])
-print(listeler[:2])
+print(listeler[:2]) 
+# burada: konulması listedeki kaç tane değerin yazdırılacağı hakkında bilgi verir
