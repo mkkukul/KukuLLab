9adf313449fb250cfdbecabf8925aa0a459d4623
mkkukul <mkkukul@gmail.com>
2024-09-09 13:45:48 +0300

    10

---
+++ Python/2.bölüm/6 Alıstirma.py
@@ -0,0 +1,4 @@
+vizePuani = int(input("Giriniz:"))
+finalPuani = int(input("Giriniz1:"))
+ortalmaHesaplama = (vizePuani+finalPuani)/2
+print(f"Vize:{vizePuani}\nFinal:{finalPuani}\nOrtalamanizi:{ortalmaHesaplama}")
