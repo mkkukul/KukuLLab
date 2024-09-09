274c2a2e2332843d88875d9f6babd33e8b6ec0f9
mkkukul <mkkukul@gmail.com>
2024-09-09 19:36:25 +0300

    22

--- Python/2.bölüm/listeler.py
+++ Python/2.bölüm/listeler.py
@@ -1,8 +1,9 @@
 listeler = ["kurt","yakup","trabzon",3500]
-listeler [1] = "tarih"
+# listeler [1] = "tarih"
+# ikinci sıradaki değeri değiştir
 # print(len(listeler))
 # print(listeler[0])
-print(listeler[:2]) 
+print(listeler) 
 # burada: konulması listedeki kaç tane değerin yazdırılacağı hakkında bilgi verir
 # listeler.append("zonguldak")
 # sonuna ekleyecek
