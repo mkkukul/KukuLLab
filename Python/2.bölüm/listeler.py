3e25a0f678753410532f7aac6dc6bf1e3ee798ff
mkkukul <mkkukul@gmail.com>
2024-09-09 19:38:27 +0300

    23

--- Python/2.bölüm/listeler.py
+++ Python/2.bölüm/listeler.py
@@ -3,7 +3,9 @@ listeler = ["kurt","yakup","trabzon",3500]
 # ikinci sıradaki değeri değiştir
 # print(len(listeler))
 # print(listeler[0])
-print(listeler) 
+# print(listeler[:2]) 
 # burada: konulması listedeki kaç tane değerin yazdırılacağı hakkında bilgi verir
 # listeler.append("zonguldak")
 # sonuna ekleyecek
+listeler [:2] = ["Trabzonspor","SK"]
+print(listeler) 
