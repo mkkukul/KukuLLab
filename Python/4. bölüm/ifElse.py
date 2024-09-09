4bf57293dcff8df6d391c77512d1ec26e07da3d2
mkkukul <mkkukul@gmail.com>
2024-09-10 00:34:16 +0300

    4

--- Python/4. bölüm/ifElse.py
+++ Python/4. bölüm/ifElse.py
@@ -1,5 +1,12 @@
-musteri = 25
-if musteri<18:
-    print("Bu mekana giremezsiniz",musteri)
+# musteri = 25
+# if musteri<18:
+#     print("Bu mekana giremezsiniz",musteri)
+# else:
+#     print("Hoşgeldiniz",musteri)
+    
+kullaniciAdiniz = input("Lütfen Adinizi Yaziniz:")
+kullaniciYasiniz = int(input("lütfen yasinizi giriniz:"))
+if kullaniciYasiniz<18:
+    print("Bu mekana giremezsiniz",kullaniciAdiniz)
 else:
-    print("Hoşgeldiniz",musteri)
+    print("Hoşgeldiniz",kullaniciAdiniz)
