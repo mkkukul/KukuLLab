93df7b8f8d0efd8eca5b579e75fad22cffd4c42b
mkkukul <mkkukul@gmail.com>
2024-09-10 05:22:56 +0300

    9

--- Python/4. bölüm/ifElse.py
+++ Python/4. bölüm/ifElse.py
@@ -4,9 +4,17 @@
 # else:
 #     print("Hoşgeldiniz",musteri)
     
-kullaniciAdiniz = input("Lütfen Adinizi Yaziniz:")
-kullaniciYasiniz = int(input("lütfen yasinizi giriniz:"))
-if kullaniciYasiniz <18:
-    print("Bu mekana giremezsiniz",kullaniciAdiniz)
+# kullaniciAdiniz = input("Lütfen Adinizi Yaziniz:")
+# kullaniciYasiniz = int(input("lütfen yasinizi giriniz:"))
+# if kullaniciYasiniz <18:
+#     print("Bu mekana giremezsiniz",kullaniciAdiniz)
+# else:
+#     print("Hoşgeldiniz",kullaniciAdiniz)
+
+sutmiktari = int(input("lütfen sut miktarini giriniz:"))
+kasarPeynirisiniri = 11
+if sutmiktari<kasarPeynirisiniri:
+    print("sut miktariniz Kasar peyniri icin uygun degil:" sutmiktari)
+    print("Kasar peyniri icin en az 11 litre sut gerekir")
 else:
-    print("Hoşgeldiniz",kullaniciAdiniz)
+    print("sut miktariniz Kasar peyniri için yeterli")
