f294b3cc45e2a186dafffee84d9099df14e0b221
mkkukul <mkkukul@gmail.com>
2024-09-10 13:23:11 +0300

    19

---
+++ Python/5. bölüm/For.py
@@ -0,0 +1,12 @@
+# liste = ["tuncay", "erol", "İstanbul", "bilişim teknolojileri"]
+# print(liste)
+# for i in liste:
+#     print(i)
+# for sayilar in range(20):
+#     print(sayilar)
+# for sayilar in range(2,50):
+#     print(sayilar)
+
+# for i in range(2, 10, 2):
+#     print(i)
+# Anlamı: start sayısından başlayarak stop sayısına kadar, her adımda step kadar ilerleyen bir dizi üretir.
--- Python/5. bölüm/while2.py
+++ Python/5. bölüm/while2.py
@@ -1,17 +1,20 @@
 dbka = "admin"
 dbps = 1234
 while True:
-    kullanıciadi= input(" Kullanıcı adını giriniz")
-    kullanıcips = int(input("şifre giriniz:"))
-    if (dbka == kullanıciadi and dbps == kullanıcips):
+    kullanıciadi= input(" Kullanıcı adınızı giriniz: ")
+    kullanıcips = int(input("şifre giriniz: "))
+    if dbka == kullanıciadi and dbps == kullanıcips:
         print("Giriş başarılı")
         break
-    elif (dbka != kullanıciadi and dbps == kullanıcips):
+    elif dbka != kullanıciadi and dbps == kullanıcips:
         print("Kullanıcı Adı Hatalı")
-    elif (dbka == kullanıciadi and dbps != kullanıcips):
+    elif dbka == kullanıciadi and dbps != kullanıcips:
         print("Kullanıcı Şifre Hatalı")
         print("şifre değiştirilsin mi? E/H")
         cevap = input()
         if cevap == "E":
             print("şifreniz Değiştiriliyor")
             yenipass = int(input("Yeni şifre giriniz:"))
+            dbps = yenipass
+    else:
+            print("kullanıcı adı ve şifresi hatalı")
