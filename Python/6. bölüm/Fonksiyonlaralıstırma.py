ee16db7a4febd0204cb615ed7700683166ff872c
mkkukul <mkkukul@gmail.com>
2024-09-11 06:21:28 +0300

    5

---
+++ Python/6. bölüm/Fonksiyonlaralıstırma.py
@@ -0,0 +1,10 @@
+def suTuketim(adı,soyadı,yas,kilo):
+    print("*"*25)
+    print(f"Adınız:{ad}\nSoyadınız:{soyadı}\nYaş:{yas}\nKilo{kilo}\n")
+    print("Günlük  litre su içmelisiniz.")
+
+ad= input("Lütfen adınızı gırınız: ")
+soyadı= input("Lütfen Soyadınızı girınız: ")
+yas=input("Lütfen Yaşınızı giriniz: ")
+kilo= input("Lütfen Kilonuzu giriniz: ")
+suTuketim(ad,soyadı,yas,kilo)
