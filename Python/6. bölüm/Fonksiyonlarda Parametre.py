5d90f6c434e8871cbace87f65fde8b8b2710f255
mkkukul <mkkukul@gmail.com>
2024-09-11 05:58:10 +0300

    3

---
+++ Python/6. bölüm/Fonksiyonlarda Parametre.py
@@ -0,0 +1,23 @@
+# ad= input("Lütfen adınızı gırınız")
+# soyadı= input("Lütfen Soyadınızı girınız")
+# yas=input("Lütfen Yaşınızı giriniz : ")
+# meslek= input("Lütfen Mesleginızı gırınız")
+
+# print(f"Adınız:{ad}\nSoyadınız:{soyadı}\nYaş:{yas}\nMeslek{meslek}\n")
+
+
+def kullanıcıBilgileri(ad,soyadı,yas,meslek):
+    print(f"Adınız:{ad}\nSoyadınız:{soyadı}\nYaş:{yas}\nMeslek{meslek}\n")
+
+
+# kullanıcıBilgileri("ali","veli",34,"fen")
+
+ad= input("Lütfen adınızı gırınız: ")
+soyadı= input("Lütfen Soyadınızı girınız: ")
+yas=input("Lütfen Yaşınızı giriniz: ")
+meslek= input("Lütfen Mesleginızı gırınız: ")
+kullanıcıBilgileri(ad,soyadı,yas,meslek)
+print(**25)
+print(kullanıcıBilgileri)
+
+
