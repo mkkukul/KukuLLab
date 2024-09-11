284bea128cc32e8b2152116a164b46444532854f
mkkukul <mkkukul@gmail.com>
2024-09-11 05:42:38 +0300

    2

--- Python/6. bölüm/Fonksiyonlar Giriş.py
+++ Python/6. bölüm/Fonksiyonlar Giriş.py
@@ -1,4 +1,13 @@
-a=5
-b=5
-toplam = a+b
-print(toplam)
+# a=5
+# b=5
+# toplam = a+b
+# print(toplam)
+
+
+def topla():
+    a=(int(input("İlk sayı giriniz")))
+    b=(int(input("İkinci sayı giriniz")))
+    toplam = a+b
+    print(toplam)
+
+topla() 
