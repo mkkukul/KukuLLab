0880a213a4f5c2c42f8f865887c033bef05131d1
mkkukul <mkkukul@gmail.com>
2024-09-12 13:05:38 +0300

    11

--- Python/7.bölüm/SozlukDevamGet.py
+++ Python/7.bölüm/SozlukDevamGet.py
@@ -2,7 +2,9 @@ lig = {"GS":"51P","FB":"53P","BJK":"57P","TS":"61P"}
 takim = input("Lütfen Puanını Öğrenmek istediğiniz Takımı Yazınız:").upper()
 # .capitalize
 # print(takim,lig[takim])
-if takim not in lig:
-    print("Seçtiğiniz Takım Listemizde Bulunmuyor")
-else:
-print(takim,":"lig[takim])
+# if takim not in lig:
+#     print("Seçtiğiniz Takım Listemizde Bulunmuyor")
+# else:
+# print(takim,":"lig[takim])
+
+print(takim,":",lig.get(takim,"Seçtiğiniz Takım Listemizde Bulunmuyor"))
