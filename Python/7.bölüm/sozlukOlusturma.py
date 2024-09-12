5bf57351d650bfb70f9d6f4457682761b410f124
mkkukul <mkkukul@gmail.com>
2024-09-12 06:00:52 +0300

    8

--- Python/7.bölüm/sozlukOlusturma.py
+++ Python/7.bölüm/sozlukOlusturma.py
@@ -1,4 +1,19 @@
 # liste = ["ali", "veli", "bilim"]
 
-sozluk = {"Apple":"Elma"}
-print(sozluk["Apple"])
+# sozluk = {"Apple":"Elma"}
+# print(sozluk["Apple"])
+
+# sozluk = {"Apple":"Elma","İsim":"Tuncay","Telefon":"Apple"}
+# print(sozluk["İsim"])
+
+# sozluk = {"Apple":"Elma","İsim":"Tuncay","Telefon":"Apple"}
+# print(len(sozluk))
+
+# sozluk = {"Apple":"Elma","İsim":"Tuncay","Telefon":"Apple"}
+# for i in sozluk:
+#     print(i)
+
+sozluk = {"Apple":"Elma","İsim":"Tuncay","Telefon":"Apple"}
+
+for isim, deger in sozluk.items():
+    print(isim, ":", deger)
