06d501392a688ff0759cc1db3c8f3acd352a9077
mkkukul <mkkukul@gmail.com>
2024-09-12 05:30:18 +0300

    2

--- Python/6. bölüm/return.py
+++ Python/6. bölüm/return.py
@@ -1,10 +1,10 @@
 # def toplam(x,y):
 #     toplam = x+y
 #     print(toplam)
-def toplam(x,y):
-    toplam = x+y
-    return x+y
+def toplam(x, y):
+    toplam = x + y
+    return x + y  # Alternatif olarak return x + y de olabilir.
 
-a = toplama(10,5)
+a = toplam(10, 5)  # toplama değil, toplam olarak düzelttik.
 b = 5
-print(a+b)
+print(a + b)
