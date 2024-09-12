625449b529b3571f00589de2f3a9963ec7a442f7
mkkukul <mkkukul@gmail.com>
2024-09-12 05:37:10 +0300

    4

--- Python/6. bölüm/VarsayılanDegerler.py
+++ Python/6. bölüm/VarsayılanDegerler.py
@@ -1,5 +1,11 @@
 
-def kullanıcı(isim,soyisim,departman):
-    print(f"İsminiz: {isim}, Soyisim: {soyisim}, Departman: {departman}")
+# def kullanıcı(isim,soyisim,departman):
+#     print(f"İsminiz: {isim}\nSoyisim: {soyisim}\nDepartman: {departman}")
 
-kullanıcı("Ahmet","Yılmaz","Bilgi İşlem")
+# kullanıcı("Ahmet","Yılmaz","Bilgi İşlem")
+
+
+def kullanıcı(isim,soyisim,departman="Boş bırakıldı"):
+    print(f"İsminiz: {isim}\nSoyisim: {soyisim}\nDepartman: {departman}")
+
+kullanıcı("Ahmet","Yılmaz", "Bilim")
