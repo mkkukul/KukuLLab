33e4beb1b5c2f3f253c132db6a32d5963a9e7397
mkkukul <mkkukul@gmail.com>
2024-09-13 05:31:49 +0300

    2

--- Python/7.bölüm/alıştırma1.py
+++ Python/7.bölüm/alıştırma1.py
@@ -1,11 +1,11 @@
 telRehberi = dict()
 def telNo(x):
     print("Numara Ekleme Ekranına Hoşgeldiniz")
-    nİsinAl = input("Lütfen Kayıt Edilecek Kişini Adını Yazınız")
-    nNoAl = input("Lütfen Telefon Numarısını Giriniz")
+    nİsinAl = input("Lütfen Kayıt Edilecek Kişini Adını Yazınız:")
+    nNoAl = input("Lütfen Telefon Numarısını Giriniz:")
     
     x = telRehberi.setdefault(nİsinAl,nNoAl)
     print(f"{nİsinAl}'adlı kişinin kayıttı başarılı")
     
     
-telNoeEkle(telRehberi)
+telNo(telRehberi)
