658e2a2148e7acad620d2cac5bc7b37985d1f9a2
mkkukul <mkkukul@gmail.com>
2024-09-13 21:18:58 +0300

    13

--- Python/7.bölüm/alıştırma1.py
+++ Python/7.bölüm/alıştırma1.py
@@ -1,37 +1,43 @@
 telRehberi = dict()
+
 def telNo(x):
     print("Numara Ekleme Ekranına Hoşgeldiniz")
-    nİsinAl = input("Lütfen Kayıt Edilecek Kişini Adını Yazınız:")
-    nNoAl = input("Lütfen Telefon Numarısını Giriniz:")
+    nIsimAl = input("Lütfen Kayıt Edilecek Kişinin Adını Yazınız: ")
+    nNoAl = input("Lütfen Telefon Numarasını Giriniz: ")
     
-    x = telRehberi.setdefault(nİsinAl,nNoAl)
-    print(f"{nİsinAl}'adlı kişinin kayıttı başarılı")
-    
-    
-# telNo(telRehberi)
+    telRehberi[nIsimAl] = nNoAl  # Kayıt doğrudan rehbere ekleniyor
+    print(f"{nIsimAl} adlı kişinin kaydı başarılı.")
+
 def telRehber(x):
     print("Rehbere Hoşgeldiniz")
-    for i,j in x.items():
+    for i, j in x.items():
         print(f"{i} : {j}")
-        
-telNo(telRehberi)       
-telRehber(telRehberi)
 
 def noSil(x):
     print("Kişi Silme Ekranına Hoşgeldiniz")
-    sKisi= input("Silinecek Kişiyi Yaızınız: ")
-    telRehberi.pop(sKisi)
-    
-while True:
-    print ("hoşgeldiniz")
-    print ("seçim yapınız")
-    secimYap = int(input("1-Ekle\n2-Sil\n3-Rehberi Gör\n"))
-    
-    if secimYap == 1:
-        telNo(telRehberi)
-    elif secimYap ==2:
-        noSil(telRehberi)
-    elif secimYap == 3:
-        telRehber(telRehberi)
+    sKisi = input("Silinecek Kişiyi Yazınız: ")
+    if sKisi in telRehberi:
+        telRehberi.pop(sKisi)
+        print(f"{sKisi} adlı kişi rehberden silindi.")
     else:
-        print("Geçersiz Seçim")
+        print(f"{sKisi} adlı kişi rehberde bulunamadı.")
+
+# Ana Döngü
+while True:
+    print("\nHoşgeldiniz!")
+    print("Seçim yapınız:")
+    
+    secimYap = input("1-Ekle\n2-Sil\n3-Rehberi Gör\n4-Çıkış\n")
+
+    # Girdi doğrulama
+    if secimYap == "1":
+        telNo(telRehberi)
+    elif secimYap == "2":
+        noSil(telRehberi)
+    elif secimYap == "3":
+        telRehber(telRehberi)
+    elif secimYap == "4":
+        print("Programdan çıkılıyor...")
+        break  # Döngüden çık
+    else:
+        print("Geçersiz Seçim! Lütfen 1, 2, 3 veya 4 giriniz.")
