8264e95f0b6d53b42a9d2da558be14acc49db0e4
mkkukul <mkkukul@gmail.com>
2024-09-13 10:19:12 +0300

    6

--- Python/7.bölüm/alıştırma1.py
+++ Python/7.bölüm/alıştırma1.py
@@ -21,3 +21,17 @@ def noSil(x):
     print("Kişi Silme Ekranına Hoşgeldiniz")
     sKisi= input("Silinecek Kişiyi Yaızınız: ")
     x = telRehberi.pop(noSil)
+    
+while True:
+    print ("hoşgeldiniz")
+    print ("seçim yapınız")
+    secimYap = int(input("1-Ekle\n2-Sil\n3-Rehberi Gör\n"))
+    
+    if secimYap == 1:
+        telNo(telRehberi)
+    elif secimYap ==2
+        noSil(telRehberi)
+    elif secimYap == 3:
+        telRehber(telRehberi)
+    else:
+        print("Geçersiz Seçim")
