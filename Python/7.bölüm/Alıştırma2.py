f82600218079da657aefad2b2233885e2ec7f1d8
mkkukul <mkkukul@gmail.com>
2024-09-14 10:03:28 +0300

    2

--- Python/7.bölüm/Alıştırma2.py
+++ Python/7.bölüm/Alıştırma2.py
@@ -2,6 +2,9 @@ tel_rehberi = dict()
 
 def tel_no_ekle(x):
     print("numara ekleme")
-    numara_no_al = input("lütfen kayıt isim:")
+    numara_isim_al = input("lütfen kayıt isim:")
     numara_no_al = input("lütfen telefon numarası giriniz:")
     x = tel_rehberi.setdefault(numara_isim_al,numara_no_al)
+    print(f"{numara_isim_al}' adlı kişi telefon listesine eklendi ...")
+    
+def tel_rehber_göster(x):
