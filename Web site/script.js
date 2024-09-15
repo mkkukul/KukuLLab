63a91712b5f40cba48995df8112f06af8165d166
mkkukul <mkkukul@gmail.com>
2024-09-15 16:19:19 +0300

    14

--- Web site/script.js
+++ Web site/script.js
@@ -1,3 +1,3 @@
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
+// function scrollToSection(id) {
+//   document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+// }
