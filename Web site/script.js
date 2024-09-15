e926056e195d961f00f95c8ec0fc550c1fe57fb7
mkkukul <mkkukul@gmail.com>
2024-09-15 16:21:10 +0300

    15

--- Web site/script.js
+++ Web site/script.js
@@ -1,3 +1,11 @@
 // function scrollToSection(id) {
 //   document.getElementById(id).scrollIntoView({ behavior: "smooth" });
 // }
+const packages = document.querySelectorAll(".package");
+
+packages.forEach((package) => {
+  package.addEventListener("click", () => {
+    packages.forEach((pkg) => pkg.classList.remove("active")); // Diğer paketleri kapat
+    package.classList.toggle("active"); // Tıklanan paketi aç/kapat
+  });
+});
