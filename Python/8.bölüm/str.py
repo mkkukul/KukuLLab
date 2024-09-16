7f488f5e810dd18d1c6dab82b5c519e73cdf6e56
mkkukul <mkkukul@gmail.com>
2024-09-16 21:20:01 +0300

    19

--- Python/8.bölüm/str.py
+++ Python/8.bölüm/str.py
@@ -10,5 +10,7 @@
 # a = random.random()
 # print(a*10,1)
 import random
-a = random.random()
-print(round(a*10,1))
+# a = random.random()
+# print(round(a*10,1))
+a= random.uniform(1.5,2.5)
+print(a)
