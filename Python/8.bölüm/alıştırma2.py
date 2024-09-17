834cef297a3639f9d92a1023159d6826e752e297
mkkukul <mkkukul@gmail.com>
2024-09-17 21:13:51 +0300

    18

--- Python/8.bölüm/alıştırma2.py
+++ Python/8.bölüm/alıştırma2.py
@@ -6,10 +6,14 @@ while True:
     seçim_yap = input("1-Notepad\n2-Excel\n3-Google\n4-Hesap Makinesi\n")
     if seçim_yap == "1":
         sp.call("notepad.exe")
+        input("Devam Edilsin Mi ?")
     elif seçim_yap == "2":
         sp.call("C:\Program Files\Microsoft Office\Office16\EXCEL.EXE")
+        input("Devam Edilsin Mi ?")
     elif seçim_yap == "3":
         sp.call("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
+        input("Devam Edilsin Mi ?")
     elif seçim_yap == "4":
         sp.call("calc.exe")
+        input("Devam Edilsin Mi ?")
         
