364987780f8d9a6b1d9ecb2894e2d8325356eb40
mkkukul <mkkukul@gmail.com>
2024-09-17 05:01:19 +0300

    1

--- Python/8.bölüm/str.py
+++ Python/8.bölüm/str.py
@@ -12,5 +12,19 @@
 import random
 # a = random.random()
 # print(round(a*10,1))
-a= random.uniform(1.5,2.5)
-print(a)
+# a= random.uniform(1.5,2.5)
+# print(a)
+# a = random.randint(20,100)
+# print(a)
+# liste = ["Ali","Ferit","Kurt"]
+# a = random.choice(liste) için rastgele seçer
+
+# print(a)
+
+liste = ["Ali","Ferit","Kurt"]
+# random.shuffle(liste)
+# print(liste)
+
+# for i in liste:
+#     print(i)
+a = random.sample(liste,2)
