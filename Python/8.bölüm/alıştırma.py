11481ffd286b708ec8772eba1ffb30e62f2ae212
mkkukul <mkkukul@gmail.com>
2024-09-17 12:24:05 +0300

    6

--- Python/8.bölüm/alıştırma.py
+++ Python/8.bölüm/alıştırma.py
@@ -5,3 +5,18 @@ def kullanıcı_ekle(x):
     print("-"*30)
     print("Kulllanıcı Ekleme Ekranı:")
     ekle = input("Lütfen Eklenecek Kullanıcıyı Yazınız:")
+    kullanıcılar.append(ekle)
+
+kullanıcı_ekle(kullanıcılar)
+# for i in kullanıcılar
+# print(i)
+
+def kullanıcı_gör(x):
+    say = 1
+    print("-"*30)
+    for i in x
+        print(str(say+"-Kullanıcı Adı:",i))
+        say+=1
+    print("-"*30)
+    
+    
