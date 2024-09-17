8c22c2ba6f02e49b02b45686368cfc9f6da69d1a
mkkukul <mkkukul@gmail.com>
2024-09-17 20:58:14 +0300

    17

--- Python/8.bölüm/alıştırma2.py
+++ Python/8.bölüm/alıştırma2.py
@@ -1,6 +1,15 @@
-import subprocess
+import subprocess as sp
 # subprocess.call("notepad.exe") notpad
 # subprocess.call("C:\Program Files\Microsoft Office\Office16\EXCEL.EXE")
 
 while True:
-    seçim_yap = input("1-Notepad\n1-Ps\n1-Google\n1-Hesap Makinesi\n") 
+    seçim_yap = input("1-Notepad\n2-Excel\n3-Google\n4-Hesap Makinesi\n")
+    if seçim_yap == "1":
+        sp.call("notepad.exe")
+    elif seçim_yap == "2":
+        sp.call("C:\Program Files\Microsoft Office\Office16\EXCEL.EXE")
+    elif seçim_yap == "3":
+        sp.call("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
+    elif seçim_yap == "4":
+        sp.call("calc.exe")
+        
