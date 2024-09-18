fa0faf649c97aca1f87ee35c74cf883aebffa0c0
mkkukul <mkkukul@gmail.com>
2024-09-18 06:12:25 +0300

    5

--- Python/9.bölüm/FinallyKavramı.py
+++ Python/9.bölüm/FinallyKavramı.py
@@ -1,5 +1,10 @@
 try:
     sayı1 = int(input("Sayı 1"))
     sayı2  = int(input("Sayı 2"))
-    toplam = sayı1/sayı2
+    toplam = sayı1+sayı2
     print(toplam)
+except ValueError:
+    print("Lütfen Sayı Giriniz")
+    
+finally:
+    print("Program Bitti")
