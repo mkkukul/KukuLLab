8b1976ebfadacc80b1a436284236138f3c462112
mkkukul <mkkukul@gmail.com>
2024-09-18 06:06:51 +0300

    2

--- Python/9.bölüm/tryExcept.py
+++ Python/9.bölüm/tryExcept.py
@@ -1,3 +1,17 @@
 
-sayı1 = int(input("Sayı 1"))
-sayı2  = int(input("Sayı 2"))
+# sayı1 = int(input("Sayı 1"))
+# sayı2  = int(input("Sayı 2"))
+
+# toplam = sayı1/sayı2
+# print(toplam)
+try:
+    sayı1 = int(input("Sayı 1"))
+    sayı2  = int(input("Sayı 2"))
+    toplam = sayı1/sayı2
+    print(toplam)
+# except ZeroDivisionError:
+#     print("Bir Sayı sıfıra Bölünemez")
+# except ValueError:
+#     print("Lütfen Sayı Giriniz")
+except(ZeroDivisionError,ValueError)
+    print("hata var")
