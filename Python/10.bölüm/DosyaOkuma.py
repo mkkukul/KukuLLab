589a7010098d1bfe30aa24e32c872c71d7e0f09c
mkkukul <mkkukul@gmail.com>
2024-09-18 12:34:16 +0300

    18

--- Python/10.bölüm/DosyaOkuma.py
+++ Python/10.bölüm/DosyaOkuma.py
@@ -6,7 +6,11 @@ import codecs as cs
 
 with cs.open("deneme.txt","r",encoding="utf-8") as dosya:
     a = dosya.readlines()
-    print(a)
+    print(a[3])
+    # say = 1
+    # for i in a:
+    #     print(say, i)
+    #     say += 1
     # a = dosya.read()
     # print(a)
     # # dosya.open("deneme.txt","r")
