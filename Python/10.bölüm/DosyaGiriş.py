863905188f26534f9a4ed39dc3053d802793626c
mkkukul <mkkukul@gmail.com>
2024-09-18 12:02:14 +0300

    14

--- Python/10.bölüm/DosyaGiriş.py
+++ Python/10.bölüm/DosyaGiriş.py
@@ -1,3 +1,6 @@
-dosya = open("deneme.txt","w")
+dosya = open("deneme.txt","r")
 
-dosya .write("Merhaba Kolsuz")
+# dosya.write("trabzonspor")
+# dosya .write("Merhaba Kolsuz")
+a = dosya.read()
+print(a)
