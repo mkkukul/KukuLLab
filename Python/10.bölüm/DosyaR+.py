8f5806da88a812895c06925109f363975d1fc107
mkkukul <mkkukul@gmail.com>
2024-09-18 13:08:58 +0300

    20

--- Python/10.bölüm/DosyaR+.py
+++ Python/10.bölüm/DosyaR+.py
@@ -4,3 +4,5 @@ with cs.open("deneme.txt","r+",encoding="utf-8") as dosya:
     # dosya.write ("\n2.Mahmut")
     dp = dosya.read()
     dosya.seek(0)
+    dp = "2.mahmut\n" + dp
+    dosya.write(dp)
