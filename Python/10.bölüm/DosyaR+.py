8608425d735fa50b6c86e764387a8880774bbe2b
mkkukul <mkkukul@gmail.com>
2024-09-18 13:32:28 +0300

    21

--- Python/10.bölüm/DosyaR+.py
+++ Python/10.bölüm/DosyaR+.py
@@ -1,8 +1,13 @@
 import codecs as cs
 
+# with cs.open("deneme.txt","r+",encoding="utf-8") as dosya:
+#     # dosya.write ("\n2.Mahmut")
+#     dp = dosya.read()
+#     dosya.seek(0)
+#     dp = "2.mahmut\n" + dp
+#     dosya.write(dp)
+
+
 with cs.open("deneme.txt","r+",encoding="utf-8") as dosya:
-    # dosya.write ("\n2.Mahmut")
-    dp = dosya.read()
-    dosya.seek(0)
-    dp = "2.mahmut\n" + dp
-    dosya.write(dp)
+    dp = dosya.readlines()
+    dp.insert(0,"2.Mahmut\n")   
