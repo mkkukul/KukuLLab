fa0e97cb0cb5de75829b354379fc07977c13ae0b
mkkukul <mkkukul@gmail.com>
2024-09-19 09:07:28 +0300

    2

--- Python/10.bölüm/alıştırmaDosyaNotDefteri.py
+++ Python/10.bölüm/alıştırmaDosyaNotDefteri.py
@@ -5,3 +5,7 @@ veri_gir = input(f"Lütfen{dosya_NEW} Dosyasına Yazılacak Veriyi Giriniz:")
 
 with codecs.open(dosya_NEW,"w", encoding="utf-8") as dosya:
     dosya.write(veri_gir)
+    soru_sor = input("Dosyasına Yazıldı. Ekleme yapmak ister Misiniz? (E/H):")
+    if soru_sor = "E":
+        open(dosya_NEW,"a")
+        yeni_veri = input("Lütfen Eklemek istiğiniz veriyi yazınız")
