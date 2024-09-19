3edae120dd3051f1d03bbf9e1cbfb550dff26e82
mkkukul <mkkukul@gmail.com>
2024-09-19 21:34:43 +0300

    18

--- Python/11. bölüm/DpXtr.py
+++ Python/11. bölüm/DpXtr.py
@@ -4,3 +4,4 @@ import sqlite3
 dp  = sqlite3.connect("kitaplar.db")
 yetki = dp.cursor()
 yetki.execute("CREATE TABLE IF NOT EXISTS kitaplar (kitap_adi TEXT, yazar_adi TEXT, yayin_evi TEXT, sayfa_sayisi INT)")
+yetki
