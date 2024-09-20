e2e08be746211f2135807bdbc48aa9fec36ba300
mkkukul <mkkukul@gmail.com>
2024-09-20 13:18:15 +0300

    13

--- Python/11. bölüm/DpXtr.py
+++ Python/11. bölüm/DpXtr.py
@@ -3,7 +3,9 @@ import sqlite3
 # Veritabanı bağlantısı oluşturuluyor
 db = sqlite3.connect("kitaplar.db")
 yetki = db.cursor()
-
+kitap_adı = input("Kitabının Asını Giriniz:")
+kitap_sayfaNosu = input("Kitabın SayfaNosu Giriniz:")
+kitap_Yılı = input("Kitabın Yılını Giriniz")
 # Tablo oluşturuluyor
 yetki.execute("""
 CREATE TABLE IF NOT EXISTS kitaplar (
