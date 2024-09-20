6eb4e13ea801d47a860142798a4d085bcc406af8
mkkukul <mkkukul@gmail.com>
2024-09-20 11:02:12 +0300

    10

--- Python/11. bölüm/DpXtr.py
+++ Python/11. bölüm/DpXtr.py
@@ -1,21 +1,11 @@
 import sqlite3
 
-# Veritabanı bağlantısı oluşturuluyor
-dp = sqlite3.connect("kitaplar.db")
-yetki = dp.cursor()
+db = sqlite3.connect("kitaplar.dp")
 
-# Tablo oluşturuluyor (isimleri düzeltildi)
-yetki.execute("""
-CREATE TABLE IF NOT EXISTS kitaplar (
-    isim TEXT,
-    sayfa_sayisi INTEGER,
-    kitap_yili INTEGER
-)
-""")
+yetki = db.cursor()
 
-# Veri ekleniyor (virgül ve nokta hatası düzeltildi)
-yetki.execute('INSERT INTO kitaplar VALUES ("Çalıkuşu", 359, 1978)')
-
-# Değişiklikleri kaydedip bağlantıyı kapatıyoruz
-dp.commit()  # commit yapılmazsa veri kalıcı olmaz
-dp.close()
+yetki.execute("CREATE TABLE IF NOT EXISTS arteta (isim,sayfasayısı,kitapyılı)")
+yetki.execute('INSERT INTO arteta VALUES("Çalıkuşu","359","1978")')
+yetki.execute('INSERT INTO arteta VALUES("Savaş ve Barış", "500", "2010")')
+db.commit()
+db.close()
