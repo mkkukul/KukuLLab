616d29e68d4b5cb0d9ef6c3e74489ee0f66482d4
mkkukul <mkkukul@gmail.com>
2024-09-20 09:01:37 +0300

    6

--- Python/11. bölüm/DpXtr.py
+++ Python/11. bölüm/DpXtr.py
@@ -3,8 +3,7 @@ import sqlite3
 
 dp  = sqlite3.connect("kitaplar.db")
 yetki = dp.cursor()
-yetki.execute("CREATE TABLE IF NOT EXISTS kitaplar (kitap_adi TEXT, yazar_adi TEXT, yayin_evi TEXT, sayfa_sayisi INT)")
+yetki.execute("CREATE TABLE IF NOT EXISTS kitaplar (isim, saysayısı, kitapyılı, )")
 yetki.execute('INSERT INTO kitaplar VALUES("Çalıkuşu","359"."1978")')
 dp.commit()
-
 dp.close()
