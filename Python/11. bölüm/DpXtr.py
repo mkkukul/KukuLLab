0c0db3203931b656408fbaade00dbaef3937d0c7
mkkukul <mkkukul@gmail.com>
2024-09-21 19:38:30 +0300

    4

--- Python/11. bölüm/DpXtr.py
+++ Python/11. bölüm/DpXtr.py
@@ -20,7 +20,9 @@ CREATE TABLE IF NOT EXISTS kitaplar (
 
 # Veri ekleniyor (parametreler kullanılarak)
 yetki.execute('INSERT INTO kitaplar VALUES (?, ?, ?)', (kitap_adı, kitap_sayfaNosu, kitap_Yılı))
-yetki.execute.fetchall()
+yetki.execute()
+
+# yetki.execute.fetchall()
 
 # Değişiklikleri kaydedip bağlantıyı kapatıyoruz
 db.commit()
