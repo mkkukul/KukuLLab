29d698dfdcc5642a7cfd7398776e55bf3f26b294
mkkukul <mkkukul@gmail.com>
2024-09-22 11:49:10 +0300

    19

--- Python/11. bölüm/işletme.py
+++ Python/11. bölüm/işletme.py
@@ -10,6 +10,7 @@ while True:
     sor = input("1-BORÇLU EKLE:\n2-BORÇLULARI GÖR\n")
     if sor == "1":
         borçlu_isim = input("Lütfen Borçlunun İsim Giriniz: ")
-        borç_miktarı = int(input("Borç giriniz: "))
-        yetki.execute("INSERT INTO kisiler VALUES(?,?)", (isim, borç))
+        borç_miktarı = int(input("Lütfen Borç Miktarını Giriniz: "))
+        yetki.execute(f"INSERT INTO kisiler VALUES('{borçlu_isim}','{borç_miktarı}')")
         db.commit()
+        print("")
