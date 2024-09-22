230a645bb6868abf4678a4a4709d459a22ecc10e
mkkukul <mkkukul@gmail.com>
2024-09-22 11:23:52 +0300

    4

--- Python/11. bölüm/DpXtr.py
+++ Python/11. bölüm/DpXtr.py
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS kitaplar (
 yetki.execute('INSERT INTO kitaplar VALUES (?, ?, ?)', (kitap_adı, kitap_sayfaNosu, kitap_Yılı))
 
 
-yetki.execute("SELECT*FROM kitaplar WHERE K ")
+yetki.execute("SELECT*FROM kitaplar WHERE kitapyili ")
 
 
 
