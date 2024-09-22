59dc27e5b0144da48dbe1f33f7ca8c73d7edd3d7
mkkukul <mkkukul@gmail.com>
2024-09-22 11:44:44 +0300

    18

--- Python/11. bölüm/işletme.py
+++ Python/11. bölüm/işletme.py
@@ -8,3 +8,8 @@ yetki.execute("CREATE TABLE IF NOT EXISITS kisiler(isim,borç)")
 while True:
     print("***VERESİYE DEFTERİNE HOŞGELDİNİZ***")
     sor = input("1-BORÇLU EKLE:\n2-BORÇLULARI GÖR\n")
+    if sor == "1":
+        borçlu_isim = input("Lütfen Borçlunun İsim Giriniz: ")
+        borç_miktarı = int(input("Borç giriniz: "))
+        yetki.execute("INSERT INTO kisiler VALUES(?,?)", (isim, borç))
+        db.commit()
