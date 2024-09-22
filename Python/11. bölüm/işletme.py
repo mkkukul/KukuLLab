6b58db7110527eff458e5679b542546f7d0055b4
mkkukul <mkkukul@gmail.com>
2024-09-22 11:39:13 +0300

    15

--- Python/11. bölüm/işletme.py
+++ Python/11. bölüm/işletme.py
@@ -2,4 +2,8 @@ import sqlite3
 
 db = sqlite3.connect("veresiye.db")
 yetki = db.cursor()
-yetki.execute("CREATE TABLE IF NOT EXISITS kisiler")
+yetki.execute("CREATE TABLE IF NOT EXISITS kisiler(isim,borç)")
+
+
+while True:
+    print("VERESİYE")
