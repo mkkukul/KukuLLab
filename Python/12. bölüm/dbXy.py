f20a9049379ceee02dc05afc79986fcbadc59534
mkkukul <mkkukul@gmail.com>
2024-09-23 21:04:25 +0300

    5

--- Python/12. bölüm/dbXy.py
+++ Python/12. bölüm/dbXy.py
@@ -2,3 +2,9 @@ class galeri:
     araç_ismi = "Ferrari"
     km_değeri = 5908
     renk = "Kırmızı"
+    
+    
+    def araba_özellikleri(self):
+        print(f"Araba ismi: {self.araç_ismi}\n")
+        print("Araba km değeri:  self.km_değeri")
+        print("Araba rengi: ", self.renk)
