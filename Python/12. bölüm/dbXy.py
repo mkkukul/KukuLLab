7dd5c5ec2ffcbb38e777bfa5e8073c3110b9bf9d
mkkukul <mkkukul@gmail.com>
2024-09-23 21:10:55 +0300

    15

--- Python/12. bölüm/dbXy.py
+++ Python/12. bölüm/dbXy.py
@@ -8,4 +8,7 @@ class galeri:
         print(f"Araba ismi: {self.araç_ismi}\nAraba km değeri: {self.km_değeri}\nAraba rengi: {self.renk}\n")
         
 şenol_otomativ = galeri()
-şenol_otomativ.araba_özellikleri()     
+şenol_otomativ.araba_özellikleri()
+fatih_otomativ = galeri()
+fatih_otomativ.araba_özellikleri
+     
