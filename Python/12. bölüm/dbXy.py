d961ecfd3d3bc446647081ef1ec0cab461a34d50
mkkukul <mkkukul@gmail.com>
2024-09-23 21:05:30 +0300

    8

--- Python/12. bölüm/dbXy.py
+++ Python/12. bölüm/dbXy.py
@@ -6,5 +6,5 @@ class galeri:
     
     def araba_özellikleri(self):
         print(f"Araba ismi: {self.araç_ismi}\n")
-        print("Araba km değeri:  {self.km_değeri}")
+        print(f"Araba km değeri: {self.km_değeri}\n")
         print("Araba rengi: {self.renk}\n")
