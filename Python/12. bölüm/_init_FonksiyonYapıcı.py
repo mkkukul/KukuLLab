76f8e1a60e7480d72f6754ce880d2cd1824840aa
mkkukul <mkkukul@gmail.com>
2024-09-24 21:02:08 +0300

    7

--- Python/12. bölüm/_init_FonksiyonYapıcı.py
+++ Python/12. bölüm/_init_FonksiyonYapıcı.py
@@ -5,4 +5,4 @@ class okul:
     mevcut = "34"
     
     def bilgileri_göster(self):
-       print("Şube:{}Öğretmen:{}Bölüm{}Sınıf Mevcudu:{}".format{self.şube,self.öğretmen,self.bölüm}) 
+       print("Şube:{}Öğretmen:{}Bölüm{}Sınıf Mevcudu:{}".format(self.şube,self.öğretmen,self.bölüm,self.mevcut)) 
