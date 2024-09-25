ea340315df483763309e9df60c7467a603e4f775
mkkukul <mkkukul@gmail.com>
2024-09-25 21:14:56 +0300

    13

--- Python/12. bölüm/_init_FonksiyonYapıcı.py
+++ Python/12. bölüm/_init_FonksiyonYapıcı.py
@@ -11,7 +11,7 @@ class okul:
         print("Şube:{}Öğretmen:{}Bölüm{}Sınıf Mevcudu:{}".format(self.şube,self.öğretmen,self.bölüm,self.mevcut))
         print("*"*45)
     
-birinci_sınıf = okul()    
+birinci_sınıf = okul("11C","Ali Çeliker", "Sporcu")    
     
 # sınıf_oluştur = okul()
 # sınıf_oluştur.bilgileri_göster()
