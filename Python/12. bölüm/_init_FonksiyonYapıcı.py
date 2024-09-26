4cdee3d1e4d3182522b04190e8e46405c7681820
mkkukul <mkkukul@gmail.com>
2024-09-26 11:24:37 +0300

    21

--- Python/12. bölüm/_init_FonksiyonYapıcı.py
+++ Python/12. bölüm/_init_FonksiyonYapıcı.py
@@ -29,20 +29,20 @@ yönetici = müdür("11", "Ömer", "Kurt", "45")
 yönetici.bilgileri_göster()
 
 # while True:
-    sınıf_adı = input("Lütfen Şube Numarasını giriniz")
-    öğretmen_bilgisi = input("Lütfen İsminizi Giriniz:")
-    bölüm_al = input("Lütfen Branşınızı Giriniz:")
-    mevcut = input("Sınıf Mevcudunu Giriniz:")
-    sınıf_oluştur = input("Sınıf Oluşturunuz:")
+#     sınıf_adı = input("Lütfen Şube Numarasını giriniz")
+#     öğretmen_bilgisi = input("Lütfen İsminizi Giriniz:")
+#     bölüm_al = input("Lütfen Branşınızı Giriniz:")
+#     mevcut = input("Sınıf Mevcudunu Giriniz:")
+#     sınıf_oluştur = input("Sınıf Oluşturunuz:")
     
-    sınıf_oluştur = okul(sınıf_adı, öğretmen_bilgisi, bölüm_al, mevcut)
-    print("Hoşgeldiniz")
-    seçim = input("Branş Değiştirmek için lütfen 1 tuşuna basınız: ")
-    if seçim == "1":
-        sınıf_oluştur.branş_değiş()
-    else:
-        print("İşlem Bitti Koçero...")
-        break
+#     sınıf_oluştur = okul(sınıf_adı, öğretmen_bilgisi, bölüm_al, mevcut)
+#     print("Hoşgeldiniz")
+#     seçim = input("Branş Değiştirmek için lütfen 1 tuşuna basınız: ")
+#     if seçim == "1":
+#         sınıf_oluştur.branş_değiş()
+#     else:
+#         print("İşlem Bitti Koçero...")
+#         break
 
 
 
