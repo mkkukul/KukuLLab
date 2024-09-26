cc28a864f2de17ece42cba57892e2faea90925ad
mkkukul <mkkukul@gmail.com>
2024-09-26 08:49:09 +0300

    1

--- Python/12. bölüm/_init_FonksiyonYapıcı.py
+++ Python/12. bölüm/_init_FonksiyonYapıcı.py
@@ -10,15 +10,17 @@ class okul:
         print("*"*45)
         print("\nŞube:{}\nÖğretmen:{}\nBölüm{}\nSınıf Mevcudu:{}\n".format(self.şube,self.öğretmen,self.bölüm,self.mevcut))
         print("*"*45)
-    def öğretmen_adı(self):
-        print("Öğretmen Adı:",self.öğretmen)
+    # def öğretmen_adı(self):
+    #     print("Öğretmen Adı:",self.öğretmen)
+    
+    def branş_değiş(self):
 
 
 
-birinci_sınıf = okul("11C ","Ali Çeliker ", "Sporcu ", "28")
-birinci_sınıf.bilgileri_göster()    
-ikinci_sınıf = okul("9C ","Mahmut Çeliker ", "Beden Eğitimi ", "22")
-ikinci_sınıf.bilgileri_göster()
-ikinci_sınıf.öğretmen_adı()    
+# birinci_sınıf = okul("11C ","Ali Çeliker ", "Sporcu ", "28")
+# birinci_sınıf.bilgileri_göster()    
+# ikinci_sınıf = okul("9C ","Mahmut Çeliker ", "Beden Eğitimi ", "22")
+# ikinci_sınıf.bilgileri_göster()
+# ikinci_sınıf.öğretmen_adı()    
 # sınıf_oluştur = okul()
 # sınıf_oluştur.bilgileri_göster()
