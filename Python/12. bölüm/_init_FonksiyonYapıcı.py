98144aaf1815c0ea1077dae17ca90cf54605d733
mkkukul <mkkukul@gmail.com>
2024-09-28 20:04:30 +0300

    17

--- Python/12. bölüm/_init_FonksiyonYapıcı.py
+++ Python/12. bölüm/_init_FonksiyonYapıcı.py
@@ -46,7 +46,7 @@ class müdür(okul):
     def zam_yap(self):
         print(f"Zam Ekranına Hoşgeldiniz Sayın{self.öğretmen}")
         zam_miktarı = int(input("Lütfen Maaş Miktarını TL cinsinden yazınız"))
-        self.maaş += zam_miktarı
+        self.maaş = self.maaş + zam_miktarı
          
 
 
