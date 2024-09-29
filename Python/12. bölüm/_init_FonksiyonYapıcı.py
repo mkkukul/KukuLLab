4dfb5b8a5447b72cc9b088dade7e5b8be66cbb82
mkkukul <mkkukul@gmail.com>
2024-09-29 10:00:01 +0300

    17

--- Python/12. bölüm/_init_FonksiyonYapıcı.py
+++ Python/12. bölüm/_init_FonksiyonYapıcı.py
@@ -54,14 +54,9 @@ class müdür(okul):
 while True:
     seçim_yap = input("1-Öğretmen Girişi\n2-Yönetici Girişi")
     
-    sınıf_oluştur = okul(sınıf_adı, öğretmen_bilgisi, bölüm_al, mevcut)
-    print("Hoşgeldiniz")
-    seçim = input("Branş Değiştirmek için lütfen 1 tuşuna basınız: ")
-    if seçim == "1":
-        sınıf_oluştur.branş_değiş()
-    else:
-        print("İşlem Bitti Koçero...")
-        break
+    
+    
+   
 
 
          
