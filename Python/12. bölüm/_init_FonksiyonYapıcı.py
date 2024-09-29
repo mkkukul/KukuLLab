f7d801b5f1b587f30d4e96dbc0b0075b959ba1f3
mkkukul <mkkukul@gmail.com>
2024-09-29 09:53:06 +0300

    13

--- Python/12. bölüm/_init_FonksiyonYapıcı.py
+++ Python/12. bölüm/_init_FonksiyonYapıcı.py
@@ -51,7 +51,21 @@ class müdür(okul):
         print(f"{self.öğretmen}'adlı öğretmenin güncel maaşı: {self.maaş}'tldir.")
         
 
-
+while True:
+    sınıf_adı = input("Lütfen Şube Numarasını giriniz")
+    öğretmen_bilgisi = input("Lütfen İsminizi Giriniz:")
+    bölüm_al = input("Lütfen Branşınızı Giriniz:")
+    mevcut = input("Sınıf Mevcudunu Giriniz:")
+    sınıf_oluştur = input("Sınıf Oluşturunuz:")
+    
+    sınıf_oluştur = okul(sınıf_adı, öğretmen_bilgisi, bölüm_al, mevcut)
+    print("Hoşgeldiniz")
+    seçim = input("Branş Değiştirmek için lütfen 1 tuşuna basınız: ")
+    if seçim == "1":
+        sınıf_oluştur.branş_değiş()
+    else:
+        print("İşlem Bitti Koçero...")
+        break
 
 
          
