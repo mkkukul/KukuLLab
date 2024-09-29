6c84e781580cd04a17ba1002389b99ab1b41f417
mkkukul <mkkukul@gmail.com>
2024-09-29 09:14:04 +0300

    1

--- Python/12. bölüm/_init_FonksiyonYapıcı.py
+++ Python/12. bölüm/_init_FonksiyonYapıcı.py
@@ -51,7 +51,7 @@ class müdür(okul):
         print(f"{self.öğretmen}'adlı öğretmenin güncel maaşı{self.maaş}")
         
          
-
+yönetici = müdür()
 
 
 # öğretmen_1 = okul("11-C","Tuncay Erol","Teknoloji","49","4500")
