d046191c707ce3ea695f5db15ba29def758c9830
mkkukul <mkkukul@gmail.com>
2024-10-01 21:23:06 +0300

    22

--- Python/12. bölüm/final.py
+++ Python/12. bölüm/final.py
@@ -61,3 +61,4 @@ sınıf_oluştur = input("Sınıf Oluşturunuz:")
 while True:
    if not kıdem_al:
        sınıf_oluştur = okul(sınıf_adı,öğretmen_bilgisi,bölüm_al,mevcut,maaş_miktarı)
+       soru_sor = 
