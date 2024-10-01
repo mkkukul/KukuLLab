9e768ef1ca623103263f3029c328cafa716658ff
mkkukul <mkkukul@gmail.com>
2024-10-01 20:55:33 +0300

    7

--- Python/12. bölüm/final.py
+++ Python/12. bölüm/final.py
@@ -51,5 +51,6 @@ sınıf_adı = input("Lütfen Şube Numarasını giriniz")
 bölüm_al = input("Lütfen Branşınızı Giriniz:")
 mevcut = input("Sınıf Mevcudunu Giriniz:")
 maaş_miktarı = input("Maaş Miktarını Giriniz:")
+print  ()
 
-
+sınıf_oluştur = input("Sınıf Oluşturunuz:")
