4804478c7b7ae1484178945d2ccfc09f93ddceef
mkkukul <mkkukul@gmail.com>
2024-10-04 10:14:36 +0300

    13

--- Projeler Python/Proje 1 Web Sitesinden Veri Çekme/Proje 1.py
+++ Projeler Python/Proje 1 Web Sitesinden Veri Çekme/Proje 1.py
@@ -3,15 +3,16 @@ from bs4 import BeautifulSoup
 
 url = requests.get("https://www.imdb.com/chart/top/")
 
-# if url.status_code == 403:
-#     print("Siteden Veri Çekilebilir")
-# else:
-#    print("Siteden Veri Çekilmez")
+if url.status_code == 403:
+    print("Siteden Veri Çekilemez (403 Forbidden)")
+elif url.status_code == 200:
+    print("Siteden Veri Çekilebilir")
+else:
+    print(f"Beklenmedik bir hata: {url.status_code}")
+soup = BeautifulSoup(url.content,"html.parser")
+print(soup)
 
-# bez = BeautifulSoup(url.content,"html.parser")
-# print(bez)
-
-print(url)
+# print(url)
 # a = url.status_code
 # print(a)   
 # a = url.content
--- Projeler Python/Proje 1 Web Sitesinden Veri Çekme/tempCodeRunnerFile.py
+++ Projeler Python/Proje 1 Web Sitesinden Veri Çekme/tempCodeRunnerFile.py
@@ -1,1 +1,12 @@
-print(url)
+import requests
+from bs4 import BeautifulSoup
+
+url = requests.get("https://www.imdb.com/chart/top/")
+
+if url.status_code == 403:
+    print("Siteden Veri Çekilebilir")
+else:
+   print("Siteden Veri Çekilmez")
+
+soup = BeautifulSoup(url.content,"html.parser")
+print(soup)
