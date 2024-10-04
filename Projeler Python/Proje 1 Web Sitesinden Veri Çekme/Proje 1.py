dca7b107b3302196283b13f4eb61bed946948214
mkkukul <mkkukul@gmail.com>
2024-10-04 10:15:22 +0300

    14

--- Projeler Python/Proje 1 Web Sitesinden Veri Çekme/Proje 1.py
+++ Projeler Python/Proje 1 Web Sitesinden Veri Çekme/Proje 1.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
+}
+
 url = requests.get("https://www.imdb.com/chart/top/")
 
 if url.status_code == 403:
