29513a4e6fd84f468975ff77e7e31f6c845eaea4
mkkukul <mkkukul@gmail.com>
2024-10-04 10:43:48 +0300

    20

--- Projeler Python/Proje 1 Web Sitesinden Veri Çekme/Proje 2.py
+++ Projeler Python/Proje 1 Web Sitesinden Veri Çekme/Proje 2.py
@@ -2,3 +2,10 @@ import requests
 from bs4 import
 
 url = requests.get("https://www.bilgisayarhocam.com/")
+if url.status_code == 200:
+    print("Siteden Veri Çekilebilir")
+else:
+    print("Siteden Veri Çekilmez")
+    
+soup = BeautifulSoup(url.content, "html.parser")
+yazdır = soup.html
