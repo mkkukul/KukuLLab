e71e89b2c7aae794d6be0e7213bf01f3927748c6
mkkukul <mkkukul@gmail.com>
2024-10-05 08:07:04 +0300

    21

--- Projeler Python/Proje 1 Web Sitesinden Veri Çekme/proje 3.py
+++ Projeler Python/Proje 1 Web Sitesinden Veri Çekme/proje 3.py
@@ -8,8 +8,8 @@ else:
     print("Siteden Veri Çekilmez")
     
 soup = BeautifulSoup(url.content, "html.parser")
-div_çek = soup.find_all("div", {"class": "post-content"}).a
-print(div_çek)
+div_çek = soup.find_all("img", {"class": "font-bold"}).a
+print(div_çek.get("href"))
 # for i in div_çek:
 #     print(i.text)
 
--- Projeler Python/Proje 1 Web Sitesinden Veri Çekme/tempCodeRunnerFile.py
+++ Projeler Python/Proje 1 Web Sitesinden Veri Çekme/tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = requests.get("https://www.imdb.com/chart/top/")
-
-if url.status_code == 403:
-    print("Siteden Veri Çekilebilir")
-else:
-   print("Siteden Veri Çekilmez")
-
-soup = BeautifulSoup(url.content,"html.parser")
-print(soup)
+"href"
