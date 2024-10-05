01f5a1877f82abebba9a688a2e1d1239acc7129a
mkkukul <mkkukul@gmail.com>
2024-10-05 07:54:54 +0300

    12

--- Projeler Python/Proje 1 Web Sitesinden Veri Çekme/proje 3.py
+++ Projeler Python/Proje 1 Web Sitesinden Veri Çekme/proje 3.py
@@ -5,4 +5,6 @@ url = requests.get("https://www.bilgisayarhocam.com/")
 if url.status_code == 200:
     print("Siteden Veri Çekilebilir")
 else:
+    print("Siteden Veri Çekilmez")
     
+soup = BeautifulSoup(url.content, "html.parser")
