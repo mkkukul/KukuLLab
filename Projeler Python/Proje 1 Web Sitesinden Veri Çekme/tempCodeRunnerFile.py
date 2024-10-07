010112957014bc6c564e27fc21d9a1cf36b177f1
mkkukul <mkkukul@gmail.com>
2024-10-07 22:14:59 +0300

    15

--- Projeler Python/Proje 1 Web Sitesinden Veri Çekme/tempCodeRunnerFile.py
+++ Projeler Python/Proje 1 Web Sitesinden Veri Çekme/tempCodeRunnerFile.py
@@ -1,1 +1,14 @@
-"href"
+import requests
+from bs4 import BeautifulSoup
+url = requests.get("https://covid19.saglik.gov.tr/")
+if url.status_code ==200:
+    print("Siteden Veri Çekilebilir")
+else:
+    print("Siteden Veri Çekilmez")
+soup = BeautifulSoup(url.content,"html.parser")
+
+for i in soup.find("il_siralama").findAll("ilsiralama_85"):
+    
+    print("************************************")
+    print(i.text)
+    print("************************************")
