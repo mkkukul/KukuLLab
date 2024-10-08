5841706633306cbf8bce48bf30f6f03b7ecd2729
mkkukul <mkkukul@gmail.com>
2024-10-08 06:05:30 +0300

    11

--- Projeler Python/Proje 1 Web Sitesinden Veri Çekme/proje 4.py
+++ Projeler Python/Proje 1 Web Sitesinden Veri Çekme/proje 4.py
@@ -1,28 +1,56 @@
 
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
 
-# Siteye istek gönder
-url = requests.get("https://covid19.saglik.gov.tr/")
-if url.status_code == 200:
-    print("Siteden Veri Çekilebilir")
-else:
-    print("Siteden Veri Çekilemez")
-    exit()
+# WebDriver'ı başlat
+driver = webdriver.Chrome()
 
-# HTML içeriğini al ve BeautifulSoup ile işle
-soup = BeautifulSoup(url.content, "html.parser")
+# Sayfayı aç
+driver.get("https://covid19.saglik.gov.tr/")
 
-# Veri çekme işlemi
-data_list = soup.find("div", class_="data_gunluk_il_listesi")
+# Sayfanın yüklenmesi için bekleyin
+time.sleep(5)
 
-# Eğer data_list boş değilse (sayfa yapısına göre kontrol)
-if data_list:
-    for i in data_list.find_all("li", class_="ilsiralama_85"):
-        print("************************************")
-        print(i.text.strip())  # Boş satırları temizlemek için strip() ekledim
-else:
-    print("Veri bulunamadı ya da çekilemiyor.")
+# Sayfanın kaynak kodunu al
+html = driver.page_source
+
+# BeautifulSoup ile işle
+soup = BeautifulSoup(html, "html.parser")
+
+# Verileri çek
+for i in soup.find("div", class_="data_gunluk_il_listesi").find_all("li", class_="ilsiralama_85"):
+    print("************************************")
+    print(i.text)
+
+# WebDriver'ı kapat
+driver.quit()
+
+
+# import requests
+# from bs4 import BeautifulSoup
+
+# # Siteye istek gönder
+# url = requests.get("https://covid19.saglik.gov.tr/")
+# if url.status_code == 200:
+#     print("Siteden Veri Çekilebilir")
+# else:
+#     print("Siteden Veri Çekilemez")
+#     exit()
+
+# # HTML içeriğini al ve BeautifulSoup ile işle
+# soup = BeautifulSoup(url.content, "html.parser")
+
+# # Veri çekme işlemi
+# data_list = soup.find("div", class_="data_gunluk_il_listesi")
+
+# # Eğer data_list boş değilse (sayfa yapısına göre kontrol)
+# if data_list:
+#     for i in data_list.find_all("li", class_="ilsiralama_85"):
+#         print("************************************")
+#         print(i.text.strip())  # Boş satırları temizlemek için strip() ekledim
+# else:
+#     print("Veri bulunamadı ya da çekilemiyor.")
 
 
 
