41f63d47a8ae329c357145b029d4f990fdab36b0
mkkukul <mkkukul@gmail.com>
2024-10-09 08:14:32 +0300

    17

--- Projeler Python/Proje 2 Digital Saat/Saat.py
+++ Projeler Python/Proje 2 Digital Saat/Saat.py
@@ -1,19 +1,23 @@
 from tkinter import Label, Tk
 import time
+
+# Uygulama penceresini oluştur
 app_windows = Tk()
-# app_windows.mainloop()
 app_windows.title("Dijital Saat")
 app_windows.geometry("500x500")
-app_windows.resizable(1,1)
+app_windows.resizable(1, 1)
 app_windows.configure(bg="black")
 
+# Yazı tipi ve renk ayarları
 text_font = ("Boulder", 68, 'bold')
 background = "black"
 foreground = "white"
-border_widht = 20
-# SAat Etiketleri
-label = Label(app_windows, font=text_font, bg=background, fg=foreground, bd=border_widht)
-label.grid(row=0,column=1,padx=10,pady=10)
-# tarih etiketi
-date_label = Label(app_windows,font=("boulder",18),bg=background,fg=foreground)
-date_label.grid(row=1,column=1,padx=10,pady=10)
+border_width = 20
+
+# Saat etiketi
+time_label = Label(app_windows, font=text_font, bg=background, fg=foreground, bd=border_width)
+time_label.grid(row=0, column=1, padx=10, pady=10)
+
+# Tarih etiketi
+date_label = Label(app_windows, font=("Boulder", 18), bg=background, fg=foreground)
+date_label.grid(row=1, column=1, padx=10, pady=10)
