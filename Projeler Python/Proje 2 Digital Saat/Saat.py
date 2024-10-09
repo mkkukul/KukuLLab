146d280feca45521853374fb4948723ee8233360
mkkukul <mkkukul@gmail.com>
2024-10-09 07:25:05 +0300

    10

--- Projeler Python/Proje 2 Digital Saat/Saat.py
+++ Projeler Python/Proje 2 Digital Saat/Saat.py
@@ -11,4 +11,6 @@ text_font = ("Boulder", 68, 'bold')
 background = "black"
 foreground = "white"
 border_widht = 20
+# SAat Etiketleri
+label = label(app_windows, font=text_font, bg=background, fg=foreground, bd=border_widht)
 
