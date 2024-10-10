f723ab687040a9f786b33bc808b559f28f613c74
mkkukul <mkkukul@gmail.com>
2024-10-10 06:11:43 +0300

    9

--- Projeler Python/Proje 3 Qr kod/QR.py
+++ Projeler Python/Proje 3 Qr kod/QR.py
@@ -3,5 +3,9 @@ from tkinter import filedialog
 import pyqrcode
 from pyqrcode import QRCode
 
-
+def qr_kodu_olustur():
+    url = qr_url.get()
+    
+    if url:
+        qr_url = pyqrcode.create(url)
 
