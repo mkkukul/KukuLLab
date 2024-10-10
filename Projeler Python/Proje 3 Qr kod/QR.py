85ccaf9a0e782aa446334bd197eaf7251caf66bf
mkkukul <mkkukul@gmail.com>
2024-10-10 06:18:22 +0300

    18

--- Projeler Python/Proje 3 Qr kod/QR.py
+++ Projeler Python/Proje 3 Qr kod/QR.py
@@ -9,4 +9,5 @@ def qr_kodu_olustur():
     if url:
         qr_url = pyqrcode.create(url)
         dosya_yolu = filedialog.asksaveasfile(defaultextension=".svg",filetypes=[("SVG Dosyaları", "*.svg")])
+        if dosya_yolu
 
