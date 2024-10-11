4d0e145ba906709a66c704009e645e34adc0c3d7
mkkukul <mkkukul@gmail.com>
2024-10-11 06:32:46 +0300

    13

--- Projeler Python/Proje 3 Qr kod/QR.py
+++ Projeler Python/Proje 3 Qr kod/QR.py
@@ -18,7 +18,7 @@ uygulama_pencere = tk.Tk()
 uygulama_pencere.title("QR Kod Oluşturucu")
 etiket = tk.Label(uygulama_pencere,text="Url'yi Girin:")
 url_girdi = tk.Entry(uygulama_pencere,width=40)
-qr_kodu_olustur_butonu = tk.Button(uygulama_pencere,text="QR")
+qr_kodu_olustur_butonu = tk.Button(uygulama_pencere,text="QR Kodu Oluştur")
 
 
 
