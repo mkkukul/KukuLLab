ad2f068ddf0d88b048ab29cbdf96b4f96b51334b
mkkukul <mkkukul@gmail.com>
2024-10-12 23:39:07 +0300

    20

--- Projeler Python/Proje 3 Qr kod/QR.py
+++ Projeler Python/Proje 3 Qr kod/QR.py
@@ -23,7 +23,7 @@ durum_etiketi = tk.Label(uygulama_pencere,text="")
 # etiket.pack()
 # qr_kodu_olustur_butonu.pack()
 etiket.grid(row=0,column=0,padx=10,pady=10)
-url_girdi.grid(row=10,column=1,padx=1,pady=1)
+url_girdi.grid(row=10,column=10,padx=1,pady=1)
 
 
 uygulama_pencere.mainloop()
