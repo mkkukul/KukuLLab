f616afafc94ff69001e9086e9944f6b04f349bcc
mkkukul <mkkukul@gmail.com>
2024-10-15 20:03:59 +0300

    22

--- Projeler Python/Proje 4 Sesli Kitap/metin okuyucu.py
+++ Projeler Python/Proje 4 Sesli Kitap/metin okuyucu.py
@@ -20,4 +20,4 @@ def metniSeseCevir(metin, dil):
 # dosya seçme fonksiyonu
 
   def   dosya_sec():
-      dosyaYolu = filedialog.askopenfilename 
+      dosyaYolu = filedialog.askopenfilename(filetypes=[("Pdf Dosyaları")]) 
