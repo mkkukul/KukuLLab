95163e80833fcb568aa10e15ab3eab2d5a722fde
mkkukul <mkkukul@gmail.com>
2024-10-19 06:08:02 +0300

    18

--- Projeler Python/Proje 5 Yüz tanıma/yüztanıma.py
+++ Projeler Python/Proje 5 Yüz tanıma/yüztanıma.py
@@ -14,5 +14,5 @@ def open_file():
        
        for(x,y,w,h) in faces:
            cv2.rectangle(img,(x,y), (x+w, y+h), (255,0,0),2)
-           cv2.putText(img,"İnsan", (x , y+h+20),)
+           cv2.putText(img,"İnsan", (x , y+h+20),cv2.FONT_HERSHEY_SIMPLEX,)
             
