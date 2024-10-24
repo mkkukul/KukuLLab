788aeb925c17cfc837d2196759c9b89d30a96326
mkkukul <mkkukul@gmail.com>
2024-10-24 11:04:41 +0300

    22

--- Projeler Python/Proje 7 Instgram Analizi/instgramanalizi.py
+++ Projeler Python/Proje 7 Instgram Analizi/instgramanalizi.py
@@ -7,10 +7,10 @@ def get_user_info(username):
         bot = instaloader.instaloader()
         profile = instaloader.Profile.from_username(bot.context,username)
     
-    user_info = {
-        "Username": profile.username,
-        "Followers": profile.followers,
-        "Followees": profile.followees,
+        user_info = {
+            "Username": profile.username,
+            "Followers": profile.followers,
+            "Followees": profile.followees,
         "Post Count": profile.mediacount,
         "Last Post Date" : get_last_post_date(profile)
         
