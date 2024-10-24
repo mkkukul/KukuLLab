e98decf44ebb7abdd38298b54aa634eec3fad03f
mkkukul <mkkukul@gmail.com>
2024-10-24 11:03:28 +0300

    21

--- Projeler Python/Proje 7 Instgram Analizi/instgramanalizi.py
+++ Projeler Python/Proje 7 Instgram Analizi/instgramanalizi.py
@@ -3,8 +3,10 @@ import tkinter as tk
 from tkinter import tkk, messagebox
 
 def get_user_info(username):
-    bot = instaloader.instaloader()
-    profile = instaloader.Profile.from_username(bot.context,username)
+    try:
+        bot = instaloader.instaloader()
+        profile = instaloader.Profile.from_username(bot.context,username)
+    
     user_info = {
         "Username": profile.username,
         "Followers": profile.followers,
