7b6e9c607704a2cdca815a3ffb8cc32090f093b4
mkkukul <mkkukul@gmail.com>
2024-10-27 09:16:26 +0300

    23

--- Projeler Python/Proje 7 Instgram Analizi/instgramanalizi.py
+++ Projeler Python/Proje 7 Instgram Analizi/instgramanalizi.py
@@ -49,7 +49,7 @@ frame = tk.Frame(root)
 frame.pack(padx=20, pady=20)
 
 label = tk.Label(root, text="Kullanıcı adı")
-label.grid(row=0,column=)
+label.grid(row=0,column=0, padx=5)
     
             
             
