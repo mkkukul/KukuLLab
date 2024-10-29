8c11c68fb4249003f3cda799a62b9f4cf02fc927
mkkukul <mkkukul@gmail.com>
2024-10-29 14:51:12 +0300

    17

--- Projeler Python/Proje 7 Instgram Analizi/instgramanalizi.py
+++ Projeler Python/Proje 7 Instgram Analizi/instgramanalizi.py
@@ -57,6 +57,6 @@ entry_username.grid(row=0,column=1,padx=5,pady=5)
 search_button = tk.Button(frame,text="Bilgileri Görüntüle",command=show_user)
 search_button.grid(row=0, column=2, padx=5,pady=5)
 
-tree = tkk.tree
+tree = tkk.Treewiw
             
             
