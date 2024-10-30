7383e58aa7df6eb972cb5d7d75965cc1eb2bbe88
mkkukul <mkkukul@gmail.com>
2024-10-30 09:32:30 +0300

    The Final 27

--- Projeler Python/Proje 8/link.py
+++ Projeler Python/Proje 8/link.py
@@ -3,23 +3,34 @@ from tkinter import messagebox
 import requests
 import pyperclip
 
-
 def shorten_url():
-    long_url = entry.get
-    responce = requests.get(f'http://tinyurl.com/api-create.php?url={long_url}')
-    short_url = responce.text
-    copy_button.confing(state=tk.NORMAL)
-    
+    long_url = entry.get()
+    response = requests.get(f'http://tinyurl.com/api-create.php?url={long_url}')  # "responce" yerine "response"
+    short_url = response.text
+    result_label.config(text=f'Kısaltılmış Link:{short_url}')
+    copy_button.config(state=tk.NORMAL)
+
 def copy_to_clipboard():
-    shorten_url = result_label.cget("text")[11:]
-    pyperclip.copy(shorten_url)
+    shortened_url = result_label.cget("text")[17:]  # "Kısaltılmış Link:" kısmını atlamak için
+    pyperclip.copy(shortened_url)
     messagebox.showinfo("Kopyalandı", "Kısaltılmış URL kopyalandı.")
-    
+
 app = tk.Tk()
 app.title("URL Kısaltıcı")
+
 label = tk.Label(app, text="URL'yi girin:")
-label.pack(pack=10)
+label.pack(pady=10)
+
 entry = tk.Entry(app, width=40)
+entry.pack()
+
 shorten_button = tk.Button(app, text="Kısalt", command=shorten_url)
+shorten_button.pack()
+
 result_label = tk.Label(app, text="")
-result_label.pack(pady=10)
+result_label.pack()
+
+copy_button = tk.Button(app, text="Kopyala", command=copy_to_clipboard, state=tk.DISABLED)
+copy_button.pack(pady=5)
+
+app.mainloop()
