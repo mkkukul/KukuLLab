9ac9a66f33f8f6926984b6e472ea410059ee1dda
mkkukul <mkkukul@gmail.com>
2024-10-30 07:09:06 +0300

    15

--- Projeler Python/Proje 8/link.py
+++ Projeler Python/Proje 8/link.py
@@ -9,5 +9,6 @@ def shorten_url():
     responce = requests.get(f'http://tinyurl.com/api-create.php?url={long_url}')
     short_url = responce.text
     copy_button.confing(state=tk.NORMAL)
-    pyperclip.copy(short_url)
-    messagebox.showinfo('Short URL', f'Short URL: {short_url}')
+    
+def copy_to_clipboard():
+    shorten_url
