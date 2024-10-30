eace979fdb4b1411f3575de933e4b3afb40b9f66
mkkukul <mkkukul@gmail.com>
2024-10-30 07:07:26 +0300

    13

--- Projeler Python/Proje 8/link.py
+++ Projeler Python/Proje 8/link.py
@@ -7,4 +7,5 @@ import pyperclip
 def shorten_url():
     long_url = entry.get
     responce = requests.get(f'http://tinyurl.com/api-create.php?url={long_url}')
-    short_url = responce.textcop
+    short_url = responce.text
+    copy_button.confing(state=tk.NORMAL)
