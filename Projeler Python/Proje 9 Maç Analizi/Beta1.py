1e44f2696aec5e61ab1d1d5cb98754e4795a5a95
mkkukul <mkkukul@gmail.com>
2024-10-31 07:05:14 +0300

    18

--- Projeler Python/Proje 9 Maç Analizi/Beta1.py
+++ Projeler Python/Proje 9 Maç Analizi/Beta1.py
@@ -8,3 +8,4 @@ def takim_bilgilerini_cek(takim):
     url = f"https://www.sporx.com/{takim}-fiksturu-ve-mac-sonuclari"
     responce = requests.get(url)
     soup = BeautifulSoup(responce.content, "html.parser")
+    maclar  = soup.find_all("div", class_="fixture")
