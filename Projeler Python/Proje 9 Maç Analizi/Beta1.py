84e620e2a6b3e9e2ae885e20ed41bc284423ab52
mkkukul <mkkukul@gmail.com>
2024-10-31 07:01:51 +0300

    16

--- Projeler Python/Proje 9 Maç Analizi/Beta1.py
+++ Projeler Python/Proje 9 Maç Analizi/Beta1.py
@@ -5,4 +5,5 @@ import os
 init(autoreset=True)
 def takim_bilgilerini_cek(takim):
     clear_screen()
-    url = f"https://www.sporx.com/trabzonspor-fiksturu-ve-mac-sonuclari"
+    url = f"https://www.sporx.com/{takim}-fiksturu-ve-mac-sonuclari"
+    responce = requests.get(url)
