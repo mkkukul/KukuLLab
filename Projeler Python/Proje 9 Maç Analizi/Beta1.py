3dfd8b71648f92cb4bcabee0e2b9c4e56d24e912
mkkukul <mkkukul@gmail.com>
2024-10-31 07:13:28 +0300

    25

--- Projeler Python/Proje 9 Maç Analizi/Beta1.py
+++ Projeler Python/Proje 9 Maç Analizi/Beta1.py
@@ -13,13 +13,16 @@ def takim_bilgilerini_cek(takim):
     toplan_gol = 0
     son_mac_skoru = None
     for mac in maclar:
-        try:
-            skor = mac.find_all("td")[3].text
-            skor = skor.split("-")
-            skor = [int(i) for i in skor]
-            toplan_gol += sum(skor)
-            if skor[0] > skor[1]:
-                galibiyet_sayisi += 1
-            son_mac_skoru = skor
-        except:
-            pass
+        skor_element = mac.find("a", class_="score")
+        
+        
+        # try:
+        #     skor = mac.find_all("td")[3].text
+        #     skor = skor.split("-")
+        #     skor = [int(i) for i in skor]
+        #     toplan_gol += sum(skor)
+        #     if skor[0] > skor[1]:
+        #         galibiyet_sayisi += 1
+        #     son_mac_skoru = skor
+        # except:
+        #     pass
