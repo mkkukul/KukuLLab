a13c103fd19cc3b4ece2584663ef7c1b70946b73
mkkukul <mkkukul@gmail.com>
2024-11-02 06:37:36 +0300

    4

--- Projeler Python/Proje 9 Maç Analizi/Beta1.py
+++ Projeler Python/Proje 9 Maç Analizi/Beta1.py
@@ -51,4 +51,4 @@ def turkce_karakter_degistir(takim_ad):
     takim_ad = takim_ad.replace("ş", "s")
     takim_ad = takim_ad.replace("ğ", "g")
     return takim_ad ;
-def son_mac_bilgilerini_cek(cek)      
+def son_mac_bilgilerini_cek(takim)      
