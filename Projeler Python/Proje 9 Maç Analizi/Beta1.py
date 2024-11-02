dfcd1393abf93dfc8d0df3f7a480437b542bd34d
mkkukul <mkkukul@gmail.com>
2024-11-02 06:58:58 +0300

    21

--- Projeler Python/Proje 9 Maç Analizi/Beta1.py
+++ Projeler Python/Proje 9 Maç Analizi/Beta1.py
@@ -74,4 +74,5 @@ def son_mac_bilgilerini_cek(takim):
                     continue
                 if mac_sayaci >= 7:
                     break
-    return son_10_mac_gol_sayilari[]
+    return son_10_mac_gol_sayilari[-14]
+def iki_takimli
