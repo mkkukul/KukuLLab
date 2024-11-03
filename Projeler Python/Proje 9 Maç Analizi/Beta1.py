de84e7af6c3cf807728333b7b21c159d7998bc11
mkkukul <mkkukul@gmail.com>
2024-11-03 06:59:12 +0300

    27

--- Projeler Python/Proje 9 Maç Analizi/Beta1.py
+++ Projeler Python/Proje 9 Maç Analizi/Beta1.py
@@ -114,6 +114,7 @@ def iki_takimli_analiz(takim1, takim2):
                         print(Fore.RED + "Veri Alınamadı Lütfen Daha Sonra Tekrar Deneyin!")
                   ortalama_gol_takim1 = sum(takım1_son_5_mac_gol) / len(takım1_son_5_mac_gol)
                   ortalama_gol_takim2 = sum(takım1_son_5_mac_gol) / len(takım1_son_5_mac_gol)
+                  
 
 
 
