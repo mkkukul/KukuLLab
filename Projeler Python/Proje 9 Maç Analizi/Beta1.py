c6784ba937675b7025f31ce8d7d5ee68550a7dd4
mkkukul <mkkukul@gmail.com>
2024-11-03 06:32:34 +0300

    5

--- Projeler Python/Proje 9 Maç Analizi/Beta1.py
+++ Projeler Python/Proje 9 Maç Analizi/Beta1.py
@@ -95,5 +95,11 @@ def iki_takimli_analiz(takim1, takim2):
             return
       galibiyet_sayisi_g2 , gol_sayisi_g2 , son_mac_skoru_g2 =  takim1_bilgilerini_cek
       sonuc += f"{takim1}\nGalibiyet Sayısı:{galibiyet_sayisi_g1}\nGol Sayısı:{gol_sayisi_g1}\nSon Maç:{son_mac_skoru_g1}"
-    return sonuc
+      sonuc += f"{takim2}\nGalibiyet Sayısı:{galibiyet_sayisi_g1}\nGol Sayısı:{gol_sayisi_g1}\nSon Maç:{son_mac_skoru_g1}"
+
+
+
+
+
+
     
