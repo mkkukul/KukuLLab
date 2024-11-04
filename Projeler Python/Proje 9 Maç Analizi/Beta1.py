7aa43aaa891b610c8cb5f7269ab04e8fe4f77118
mkkukul <mkkukul@gmail.com>
2024-11-04 06:42:04 +0300

    24

--- Projeler Python/Proje 9 Maç Analizi/Beta1.py
+++ Projeler Python/Proje 9 Maç Analizi/Beta1.py
@@ -142,7 +142,7 @@ while True:
       takim1 = input("Lütfen ev sahibi takımı girin:")
       takim2 = input("Lütfen deplasman takımını girin :")
       iki_takimli_analiz(takim1,takim2)
-      devam
+      devam =input
                   
 
 
