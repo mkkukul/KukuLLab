edf9e6d050f10256ba0603716d6354d495aeb54a
mkkukul <mkkukul@gmail.com>
2024-11-04 06:41:13 +0300

    20

--- Projeler Python/Proje 9 Maç Analizi/Beta1.py
+++ Projeler Python/Proje 9 Maç Analizi/Beta1.py
@@ -138,7 +138,7 @@ def iki_takimli_analiz(takim1, takim2):
 
       print(sonuc)
 
-if __name__ == "__main__":
+
       takim1 = input("Lütfen ev sahibi takımı girin:")
       takim2 = input("Lütfen deplasman takımını girin :")
       iki_takimli_analiz(takim1,takim2)
