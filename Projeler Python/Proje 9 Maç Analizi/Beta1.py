207d053ad190dfaf7391006309b3febb3354b40e
mkkukul <mkkukul@gmail.com>
2024-11-04 05:47:54 +0300

    10

--- Projeler Python/Proje 9 Maç Analizi/Beta1.py
+++ Projeler Python/Proje 9 Maç Analizi/Beta1.py
@@ -119,6 +119,8 @@ def iki_takimli_analiz(takim1, takim2):
                   gol_tahmini   = ortalama_gol +0.25 if takım1_son_5_mac_gol[-1] > takım2_son_5_mac_gol[-1] else ortalama_gol
                   sonuc += f"{Fore.GREEN} Maçta Muhtemelen {gol_tahmini:.2f} gol olacak.\n"
             print(sonuc)
+
+if__name__ == "__main__":
                   
 
 
