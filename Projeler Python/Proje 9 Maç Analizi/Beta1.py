c6e3a4c091a2d1815ba796e8d14e5d2fb730572f
mkkukul <mkkukul@gmail.com>
2024-11-04 05:42:42 +0300

    4

--- Projeler Python/Proje 9 Maç Analizi/Beta1.py
+++ Projeler Python/Proje 9 Maç Analizi/Beta1.py
@@ -116,7 +116,7 @@ def iki_takimli_analiz(takim1, takim2):
                   ortalama_gol_takim2 = sum(takım1_son_5_mac_gol) / len(takım1_son_5_mac_gol)
                   
                   ortalama_gol = (ortalama_gol_takim1 + ortalama_gol_takim2) / 2
-                  gol_tahmini   = ortalama_gol +0.25 if 
+                  gol_tahmini   = ortalama_gol +0.25 if takım1_son_5_mac_gol[-1] > takım2_son_5_mac_gol[-1] else ortalama_gol
 
 
 
