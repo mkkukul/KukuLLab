f3b8a6faf13e2d62789f355d6efa88c9c99f58bd
mkkukul <mkkukul@gmail.com>
2024-11-05 21:24:15 +0300

    21

--- Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
+++ Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
@@ -6,8 +6,8 @@ class StokTakipUygulaması:
     def __init__(self,root):
         self.root = root
         self.root.title("Stok Takip Uygulaması")
-        self.root.geometry("800x600")
-        self.root.configure(bg="#f0f0f0")
+        # self.root.geometry("800x600")
+        # self.root.configure(bg="#f0f0f0")
 
         self.conn = sqlite3.connect("stok_takip.db")
         self.cursor = self.conn.cursor()
