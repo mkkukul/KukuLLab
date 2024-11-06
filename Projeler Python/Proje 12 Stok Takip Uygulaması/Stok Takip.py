86ef96d76d93cb079f4125e82c4296f97d262613
mkkukul <mkkukul@gmail.com>
2024-11-06 10:12:54 +0300

    22

--- Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
+++ Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
@@ -36,7 +36,7 @@ class StokTakipUygulaması:
         self.adet_label = tk.Label(root, text="Ürün Adı:")
         self.urun_adi_label.grid(row=1, column=0)
         self.adet_label = tk.Entry(root)
-        self.
+        self.adet_label.grid
         
         
         self.birim_fiyat_label = tk.Label(root, text="Birim Fiyat:")
