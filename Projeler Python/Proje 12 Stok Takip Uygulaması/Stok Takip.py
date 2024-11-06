fc15f782426164b747fe5adb22bb0b2b989bf44b
mkkukul <mkkukul@gmail.com>
2024-11-06 08:27:31 +0300

    3

--- Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
+++ Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
@@ -24,7 +24,7 @@ class StokTakipUygulaması:
         self.conn.commit()
         self.id_label = tk.Label(self.root, text="ID:")
         self.id_label.grid(row=0, column=0, padx=10, pady=10)
-        self.id_entry
+        self.id_entry = tk.Entry(self.root)
         
         # self.urun_adi_var = tk.StringVar()
         # self.stok_miktari_var = tk.IntVar()
