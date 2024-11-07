bdc8f8a86438ff7e69692b828ba5533468d42208
mkkukul <mkkukul@gmail.com>
2024-11-07 10:19:29 +0300

    4

--- Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
+++ Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
@@ -45,6 +45,7 @@ class StokTakipUygulaması:
         self.birim_fiyat_entry.grid(row=3, column=1)
         
         self.ekle_button = tk.Button(root, text="Ekle", command=self.ekle)
+        self.ekle_
         
         
         
