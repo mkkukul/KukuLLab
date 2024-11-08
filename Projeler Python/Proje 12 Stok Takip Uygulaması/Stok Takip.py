c1ec04a81cdfa81d430c8d968e1b703d256e67f6
mkkukul <mkkukul@gmail.com>
2024-11-08 19:33:37 +0300

    2

--- Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
+++ Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
@@ -56,7 +56,7 @@ class StokTakipUygulaması:
         self.temizle_button = tk.Button(root, text = "Temizler", command=self.girisleri_temizle)
         self.temizle_button.grid(row=4, column=3, columnspan=1)
         
-        self.arama
+        self.arama_label
         
         
         
