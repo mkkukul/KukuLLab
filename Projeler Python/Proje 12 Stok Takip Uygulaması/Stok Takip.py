6ea9d8bbd8c28cf4676e419465dce043b13790c5
mkkukul <mkkukul@gmail.com>
2024-11-10 20:28:29 +0300

    14

--- Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
+++ Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
@@ -108,7 +108,7 @@ class StokTakipUygulaması:
             self.id_entry.delete(0, tk.END)
             self.id_entry.insert(0, values[0])
             self.urun_adi_entry.delete(0, tk.END)
-        self.urun_adi_entry.insert(0, values[1])
+            self.urun_adi_entry.insert(0, values[1])
         self.adet_label.delete(0, tk.END)
         self.adet_label.insert(0, values[2])
         self.birim_fiyat_entry.delete(0, tk.END)
