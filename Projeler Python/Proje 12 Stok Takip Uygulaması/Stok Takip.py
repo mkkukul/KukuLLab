7aa6cbf80ffc70290b0db402a6806a5f25e926c0
mkkukul <mkkukul@gmail.com>
2024-11-10 20:24:55 +0300

    4

--- Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
+++ Projeler Python/Proje 12 Stok Takip Uygulaması/Stok Takip.py
@@ -88,7 +88,7 @@ class StokTakipUygulaması:
         self.girisleri_temizle()
     def girisleri_temizle(self):
         self.id_entry.delete(0,tk.END)
-    self.urun_adi_entry.delete(0, tk.END)
+        self.urun_adi_entry.delete(0, tk.END)
     self.adet_label.delete(0, tk.END)
     self.birim_fiyat_entry.delete(0, tk.END)
 def arama(self,event):
