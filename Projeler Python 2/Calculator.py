c711f9d58661aaa09f12dcb157b326f0da8cf20e
mkkukul <mkkukul@gmail.com>
2024-11-13 06:37:32 +0300

    7

--- Projeler Python 2/Calculator.py
+++ Projeler Python 2/Calculator.py
@@ -1,6 +1,10 @@
 from tkinter import Tk, Entry, Button, StringVar
 
 class Calculator:
+    def __init__(self,master):
+        master.title("Calculator")
+        master.geometry("357x420+0+0")
+        
 
 
 
